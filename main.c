d4a9cd1b2f710d511fe096d9170bde4de37d7abd
Ajil Reji <ajilreji95@gmail.com>
2026-07-09 21:38:47 +0530

    reciver

--- main.c
+++ main.c
@@ -1,96 +1,53 @@
 /*
- * Heat sensor.c
+ * receiver.c
  *
- * Created: 6/10/2026 9:58:08 PM
+ * Created: 6/11/2026 9:34:37 PM
  * Author : ASUS
  */ 
 
-#define F_CPU 16000000UL 
+#define F_CPU 16000000UL
+
 #include <avr/io.h>
 #include <util/delay.h>
-#include "LCD.h"
 
 int main(void)
 {
-   lcd_init();
-   
-   
-   DDRC &= ~(1<<PC3);   // PC3 (ADC3) as input
-   
-   ADMUX|=(1<<REFS0);
-   
-   //ADMUX&=~(1<<REFS1);  //Set reference voltage to external 5v
-   
-   ADMUX|=(1<<MUX1);
-   ADMUX|=(1<<MUX0);
-   
-   ADCSRA|=(1<<ADEN);
-   
-   //ADCSRA |=(1<<ADSC);
-   
-   ADCSRA |= (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);
-   //char tempStr[10];
+	UBRR0H = 0;
+	UBRR0L = 103;
 
-    while (1) 
-    {	ADCSRA |=(1<<ADSC);
-		
-		
-	    while(ADCSRA & (1<<ADSC));
-		
-	    uint16_t adcread = ADC;
-		float voltage = (adcread * 5.0) / 1024.0;
-		float temperature = voltage *100.0;
-	    
-	    uint16_t temp_int = (uint16_t)temperature;
-	    uint8_t temp_dec = (uint8_t)((temperature - temp_int) * 10);
+	// Enable Receiver and Transmitter
+	UCSR0B = (1 << RXEN0) | (1 << TXEN0);
 
-		lcd_clear();
-        lcd_set_cursor(0, 0);
-        lcd_print("Temp:");
+	// USART configuration:
+	// Asynchronous, No parity, 1 stop bit, 8 data bits
+	UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+	
+	char str[6];
+	uint8_t i = 0;
 
-        lcd_set_cursor(0, 6);
-        lcd_print_uint16(temp_int);
-        lcd_print(".");
-        lcd_print_uint16(temp_dec);
-        lcd_print("C");
+	while (1)
+	{
+		// Wait for a character to be received
+		while (!(UCSR0A & (1 << RXC0)));
 
-        lcd_set_cursor(1, 0);
-        lcd_print("ADC:");
-        lcd_print_uint16(adcread);
+		// Read received character
+		char data = UDR0;
 
-        _delay_ms(1000);
-		DDRD |= (1<<DDD6);
-		if ( temp_int>45)
-		{ 
-		DDRB|=(1<<DDB5);
-	    PORTB|=(1<<PB5);
-		
-		
-		DDRD |= (1<<DDD6); //////////////////////////////////////////////////////////////////////////
-		
-		//FAST PWM
-		TCCR0A |= (1<<WGM00);
-		TCCR0A |= (1<<WGM01);
-		//NON INVERTING PWM
-		TCCR0A &= ~(1<<COM0A0);
-		TCCR0A |= (1<<COM0A1);
-		//PRESCALAR TIMER CLK/1024
-		TCCR0B &=~(1<<CS02);
-		TCCR0B |=(1<<CS01);
-		TCCR0B |=(1<<CS00);
-		
-		OCR0A = 154 ; //////////////////////////////////////////////////////////////////////////
-		}
-		if (temp_int<45)
+		// Store first 5 characters
+		if (i < 5)
 		{
-		PORTB&=~(1<<PB5);
-		
-		
-		DDRD &=~ (1<<DDD6);
-		}
-		
-		
-		
-    }
-}
+			str[i] = data;
+			i++;
 
+			if (i == 5)
+			{
+				str[5] = '\0';   // Complete string
+				i = 0;          
+			}
+		}
+
+		// Echo the received character back
+		while (!(UCSR0A & (1 << UDRE0)));
+		UDR0 = data;
+	}
+}
